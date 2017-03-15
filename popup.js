a702f43f709b2bd193ffcbe44c6c0d3548e4830d
ethanchewy <17chiue@gmail.com>
2017-03-15 17:19:35 -0400

    Added block name and question append functionality. Look at test.js

--- popup.js
+++ popup.js
@@ -1,7 +1,53 @@
-//Button Clicks
+//Global Vars
+var activeTab;
+function myfunc(){
+    var x = $('#options option:selected').text();
+    chrome.extension.sendMessage({sel_text: x});
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
     $( "#run_game" ).click(function() {
-      console.log("run game pressed");
-    });
+		console.log("sdf");
+		//chrome.tabs.create({url: 'index.html'});
+		/*
+		chrome.tabs.executeScript(null, {
+	        //code: 'var config = ' + JSON.stringify(getKeywords)
+	    }, function() {
+	        chrome.tabs.executeScript(null, {file: 'custom.js'});
+	    });
+		chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabs) {
+            activeTab = tabs[0];
+            get_input_values();
+        });
+        */
+        $('#options').change(myfunc);
+        var someVar = {text: 'test', foo: 1, bar: false};
+		chrome.tabs.executeScript({
+		    code: '(' + function(params) {
+		        document.body.insertAdjacentHTML('beforeend',
+		            '<div style="all:unset; position:fixed; left:0; top:0; right:0; bottom:0;' +
+		                'background-color:rgba(0,255,0,0.3)">' + params.text + '</div>'
+		        );
+		        return {success: true, html: document.body.innerHTML};
+		    } + ')(' + JSON.stringify(someVar) + ');'
+		}, function(results) {
+		    console.log(results[0]);
+		});
+	});
+
+	function get_input_values() {
+	    chrome.tabs.sendMessage(activeTab.id, {
+	        "method": "get_data"
+	    }, function(items) {
+	        overlay_items();
+	    });
+	}
+
+
+	function overlay_items(){
+
+	}
 });
---
+++ test.js
@@ -0,0 +1,201 @@
+var lastRequestTime = 0;
+var requestInterval = 50;
+
+var timeoutHistory = [];
+var xhrHistory = [];
+
+function get(url, done) {
+	var xhr = new XMLHttpRequest();
+	xhrHistory.push(xhr);
+	xhr.open('GET', url, true);
+	xhr.onreadystatechange = function (e) {
+		if (xhr.readyState == 4) {
+			done(xhr.responseText);
+		}
+	}
+	var delay = Math.max(lastRequestTime + requestInterval - (+new Date()), 0) + Math.random() * requestInterval;
+	lastRequestTime = delay + (+new Date());
+	timeoutHistory.push(setTimeout(function () {
+		xhr.send();
+	}, delay));
+}
+
+
+function getNewsFeedFrequency(maxDepth, done, onFetch) {
+	var frequency = {};
+	var newdiv = document.createElement("DIV");
+	url = document.location.href;
+	console.log(document.location.href);
+	//document.write("sdfsdfsdfdfs");
+	document.body.append("<div>sdfsdfsdfdf</div>");
+	$(".cb .cc").text("TESTTESTTEST");
+
+	//document.write("sddfdssdfsd");
+	console.log("HELLO_1");
+	function fetch(url, depth, fetchDone) {
+		console.log('getNewsFeedFrequency.fetch', depth);
+		get(url, function (text) {
+			var $t = $(text);
+
+			var links = $t.find('[role="article"] a').map(function () {
+				return /(.*?)(?:\/\?|\?|$)/.exec($(this).attr('href'))[1];
+			}).get();
+			/*
+			var htmlstuff = $t.find('[role="article"] a').map(function () {
+				console.log("sdff");
+				$(this).append("<p>TEST</p>")[1];
+				return 0;
+			}).get();
+			*/
+			/*
+			var htmlstuff = $t.find('[role="article"] a').map(function () {
+				return /(.*?)(?:\/\?|\?|$)/.exec($(this).attr('class'))[1];
+			}).get();
+			*/
+			var g = 1;
+			$('[role="article"]').each(function(i) {
+				//$(this).attr('id', 'page'+(i+1));
+				if ($(this).find(".cg.ch").length > 0){
+					console.log("g: " + g);
+					//$(this).find(".cg.ch").append('<div class=\"question'+g+'\">Question ' + g +'</div>');
+					//$(this).after('<div id=\"question'+g+'\"'+" class=\"question\">"+ 'Question ' + g +'</div>');
+					//Append Question
+					$(this).append('<div id=\"question'+g+'\"'+" class=\"question\">"+ 'Question ' + g +'</div>');
+					
+					//Block Out Name
+					$(this).find("span:first").css('background-color', 'red');
+					$(this).find("span:first").text("GUESS!");
+
+					g++;
+				}
+				
+			});
+			/*
+			htmlstuff.forEach(function (html) {
+				if (!frequency.hasOwnProperty(html)) frequency[html] = 0;
+				frequency[html]++;
+				$(".cb .cc").append("<p>TEST</p>");
+			});
+			*/
+			/*
+			htmlstuff.forEach(function (html) {
+				if (!frequency.hasOwnProperty(html)) frequency[html] = 0;
+				frequency[html]++;
+				if(html=="cg ch"){
+					$(".cb.cc").prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
+				}
+				
+			});
+			*/
+			//$(".cb").prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
+			//console.log(htmlstuff);
+			
+			links.forEach(function (link) {
+				if (!frequency.hasOwnProperty(link)) frequency[link] = 0;
+				frequency[link]++;
+				console.log("HELLO_2");
+				console.log(link);
+
+				//$('a[href^='+link+"http://example.com/external/link"]').prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
+				/*
+				var linkdiv = document.createElement("DIV");
+				linkdiv.appendChild(document.createTextNode("some text"));
+				link.appendChild(linkdiv);
+				*/
+
+			});
+
+			onFetch();
+			fetchDone();
+			/*
+			var next = $t.find('a[href^="/stories.php?aftercursorr"]').last().attr('href');
+			if (next && depth) {
+				fetch('https://mbasic.facebook.com' + next, depth - 1, fetchDone);
+			} else {
+				fetchDone();
+			}
+			*/
+		});
+	}
+
+	fetch(url, maxDepth, function () {
+		done(frequency);
+	});
+}
+
+$( document ).ready(function() {
+	console.log("sdf");
+	var maxNewsFeedDepth = 20;
+	var profileToFrequency;
+	//document.body.append("<div>sdfsdfsdfdf</div>");
+	//chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
+	getNewsFeedFrequency(maxNewsFeedDepth, function (data, progress) {
+		//chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
+		//chrome.extension.getBackgroundPage().console.log(data);
+		chrome.runtime.sendMessage({
+			action: "parseResponse",
+			data: data,
+			tab: sender.tab.id
+		});
+		//profileToFrequency = data;
+		//onReturn();
+	},  function (elapsed, total) {
+		console.log('Progress: ' + elapsed + '/' + total);
+		chrome.runtime.sendMessage({
+			action: "parseProgress",
+			data: {
+				elapsed: elapsed,
+				total: total,
+			},
+		});
+	});
+});
+/*
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
+	var maxNewsFeedDepth = 20;
+	var profileToFrequency;
+	getNewsFeedFrequency(maxNewsFeedDepth, function (data, progress) {
+		chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
+		chrome.extension.getBackgroundPage().console.log(data);
+		chrome.runtime.sendMessage({
+			action: "parseResponse",
+			data: data,
+			tab: sender.tab.id
+		});
+		//profileToFrequency = data;
+		//onReturn();
+	},  function (elapsed, total) {
+		console.log('Progress: ' + elapsed + '/' + total);
+		chrome.runtime.sendMessage({
+			action: "parseProgress",
+			data: {
+				elapsed: elapsed,
+				total: total,
+			},
+		});
+	});
+});
+*/
+/*
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	var bkg = chrome.extension.getBackgroundPage();
+	bkg.console.log('foo');
+    appendTextToBody(request.sel_text);
+  });
+
+function appendTextToBody(text) {
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.executeScript(tab.id, {"code" : '$("body").append("Test : "'+text+');'}) ;
+	chrome.tabs.executeScript(null, {file: 'custom.js'});  
+  });
+}
+
+function check(tab_id, data, tab){
+    if(tab.url.indexOf("google") > -1){
+        chrome.pageAction.show(tab_id);
+    }
+};
+chrome.tabs.onUpdated.addListener(check);
+*/
