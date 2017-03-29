bf4e97647f5a4848d162c0f20d86bfc30e313176
ethanchewy <17chiue@gmail.com>
2017-03-28 20:26:08 -0400

    Added randomization function to get 3 friends to put in the options using jQuery deferred objects

--- test.js
+++ test.js
@@ -23,8 +23,11 @@ function get(url, done) {
 }
 
 //Get list of friends based on the most frequent posters on news feed
+//Promise to be resolved using deferred objects inside the getnewsfeed function
+var promises = [];
 function getFriends(){
-	$.ajax({
+	var friends = [];
+	request = $.ajax({
 	     url: "https://mbasic.facebook.com/me/friends",
 	     dataType: 'text',
 	     success: function(data) {
@@ -37,12 +40,16 @@ function getFriends(){
 	               index = name.indexOf(firstDigit);
 	               name = name.slice(0, index);
 	               // Do something here
-	               console.log(name);
+	               friends.push(name);
 	          }
 
-	          console.log(elements);
+	          //console.log(friends);
+	          
 	     }
 	});
+	promises.push( request);
+	return friends;
+
 }
 
 //See if the answer is correct
@@ -54,7 +61,18 @@ var total_score=0;
 
 //Create a way to show mainly friend's contact
 
-
+//From http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
+//Get subarray for questions
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
 
 
 //Create hash table for article objects
@@ -64,42 +82,32 @@ function getNewsFeedFrequency(maxDepth, done, onFetch) {
 	var frequency = {};
 	var newdiv = document.createElement("DIV");
 	url = document.location.href;
-	console.log(document.location.href);
-	//document.write("sdfsdfsdfdfs");
-	/*
-	document.body.append("<div>sdfsdfsdfdf</div>");
-	$(".cb .cc").text("TESTTESTTEST");
-	*/
+	//console.log(document.location.href);
+	//console.log("HELLO_1");
 
-	//document.write("sddfdssdfsd");
-	console.log("HELLO_1");
-	function fetch(url, depth, fetchDone) {
-		console.log('getNewsFeedFrequency.fetch', depth);
+function fetch(url, depth, friends_list, fetchDone) {
+		//console.log('getNewsFeedFrequency.fetch', depth);
+		//Get the three friends that will be inserted to the questions
+		
+		console.log(friends_list);
+		
 		get(url, function (text) {
 			var $t = $(text);
 
+			//Get any link that contains an article witha preview window
 			var links = $t.find('[role="article"] a').map(function () {
 				return /(.*?)(?:\/\?|\?|$)/.exec($(this).attr('href'))[1];
 			}).get();
-			/*
-			var htmlstuff = $t.find('[role="article"] a').map(function () {
-				console.log("sdff");
-				$(this).append("<p>TEST</p>")[1];
-				return 0;
-			}).get();
-			*/
-			/*
-			var htmlstuff = $t.find('[role="article"] a').map(function () {
-				return /(.*?)(?:\/\?|\?|$)/.exec($(this).attr('class'))[1];
-			}).get();
-			*/
+
 			var g = 1;
+
+
+
+			//See if each article contains a link or not
 			$('[role="article"]').each(function(i) {
 				//$(this).attr('id', 'page'+(i+1));
 				if ($(this).find("[role=\"presentation\"]").length > 0){
-					console.log("g: " + g);
-					//$(this).find(".cg.ch").append('<div class=\"question'+g+'\">Question ' + g +'</div>');
-					//$(this).after('<div id=\"question'+g+'\"'+" class=\"question\">"+ 'Question ' + g +'</div>');
+
 					//Append Question
 					$(this).append('<div id=\"question'+g+'\"'+" class=\"question\">"+ 
 						'Question '
@@ -109,37 +117,26 @@ function getNewsFeedFrequency(maxDepth, done, onFetch) {
 					
 					//Block Out Name
 					$(this).find("span:first").css('background-color', 'red');
-					//$(this).find("span:first").text("GUESS!");
-					/*
-					var me = $(this).find("span:first");
-					me.html(me.html().replace(/^(\w+)/, '<span>test</span>'));
-					*/
-					/*
-					var me = $(this).find("span:first");
-					$(me).html(function (i, html) {
-					    return html.replace(/(\w+\s\w+)/, 'TEST');
-					});
-					*/
+
 					var re = /^([a-z]+)[\s,;:]+([a-z]+)/i; 
 					var str = $(this).find("span:first").text();
-					console.log(str);
+					//console.log(str);
 					var m;
 					var rest = str.split(" ");
-					console.log(rest);
+					//console.log(rest);
 
 					var name = rest.slice(0, 2);
 					name = name.join(" ");
-					console.log(name);
+					//console.log(name);
 
 					//ReProgram with a yield statement or a test fail loop
 					if ((m = re.exec(str)) !== null) {
 						if(rest.length>2){
 							rest.splice(0, 2);
-							$(this).find("span:first").text("Who Is This " + rest.join(" "));
+							$(this).find("span:first").text("Who Is This? " + rest.join(" "));
 						} else{
-							$(this).find("span:first").text("Who Is This");
+							$(this).find("span:first").text("Who Is This?");
 						}
-					    
 					}
 
 					//Block out profile photo + hovering function
@@ -167,63 +164,40 @@ function getNewsFeedFrequency(maxDepth, done, onFetch) {
 			//Change questions as well
 			//Look at extension code for sponsored posts to avoid marking those up. 
 			//Filter out photos posted => c=any class similarity?
+			//Can't add total score for now since this refreshes each time. Could add later
+			// ^currently works so the user can check immedietly to see if they know who shared it. 
 
-
-			/*
-			htmlstuff.forEach(function (html) {
-				if (!frequency.hasOwnProperty(html)) frequency[html] = 0;
-				frequency[html]++;
-				$(".cb .cc").append("<p>TEST</p>");
-			});
-			*/
-			/*
-			htmlstuff.forEach(function (html) {
-				if (!frequency.hasOwnProperty(html)) frequency[html] = 0;
-				frequency[html]++;
-				if(html=="cg ch"){
-					$(".cb.cc").prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
-				}
-				
-			});
-			*/
-			//$(".cb").prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
-			//console.log(htmlstuff);
 			
 			links.forEach(function (link) {
 				if (!frequency.hasOwnProperty(link)) frequency[link] = 0;
 				frequency[link]++;
-				console.log("HELLO_2");
-				console.log(link);
-
-				//$('a[href^='+link+"http://example.com/external/link"]').prepend('<div id="root"> dsfsdfsdfsdfsdfs</div>');
-				/*
-				var linkdiv = document.createElement("DIV");
-				linkdiv.appendChild(document.createTextNode("some text"));
-				link.appendChild(linkdiv);
-				*/
 
 			});
 
 			onFetch();
 			fetchDone();
-			/*
-			var next = $t.find('a[href^="/stories.php?aftercursorr"]').last().attr('href');
-			if (next && depth) {
-				fetch('https://mbasic.facebook.com' + next, depth - 1, fetchDone);
-			} else {
-				fetchDone();
-			}
-			*/
 		});
 	}
 
-	fetch(url, maxDepth, function () {
-		done(frequency);
+	//Get random sort of 3 friends
+	friends = getFriends();
+
+	//http://stackoverflow.com/a/20291749/4698963
+	$.when.apply(null, promises).done(function(){
+		var questions_friends = getRandomSubarray(friends, 3);
+		fetch(url, maxDepth, questions_friends, function () {
+			done(frequency);
+		});
 	});
+	
+
+	
+
+	
 }
 
 $( document ).ready(function() {
-	getFriends();
+	//var friends = getFriends();
 
 	//console.log("sdf");
 	var maxNewsFeedDepth = 20;
@@ -241,7 +215,7 @@ $( document ).ready(function() {
 		//profileToFrequency = data;
 		//onReturn();
 	},  function (elapsed, total) {
-		console.log('Progress: ' + elapsed + '/' + total);
+		//console.log('Progress: ' + elapsed + '/' + total);
 		chrome.runtime.sendMessage({
 			action: "parseProgress",
 			data: {
@@ -253,52 +227,3 @@ $( document ).ready(function() {
 
 
 });
-/*
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
-	var maxNewsFeedDepth = 20;
-	var profileToFrequency;
-	getNewsFeedFrequency(maxNewsFeedDepth, function (data, progress) {
-		chrome.extension.getBackgroundPage().console.log("sdfsdfsfdsdf");
-		chrome.extension.getBackgroundPage().console.log(data);
-		chrome.runtime.sendMessage({
-			action: "parseResponse",
-			data: data,
-			tab: sender.tab.id
-		});
-		//profileToFrequency = data;
-		//onReturn();
-	},  function (elapsed, total) {
-		console.log('Progress: ' + elapsed + '/' + total);
-		chrome.runtime.sendMessage({
-			action: "parseProgress",
-			data: {
-				elapsed: elapsed,
-				total: total,
-			},
-		});
-	});
-});
-*/
-/*
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	var bkg = chrome.extension.getBackgroundPage();
-	bkg.console.log('foo');
-    appendTextToBody(request.sel_text);
-  });
-
-function appendTextToBody(text) {
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.executeScript(tab.id, {"code" : '$("body").append("Test : "'+text+');'}) ;
-	chrome.tabs.executeScript(null, {file: 'custom.js'});  
-  });
-}
-
-function check(tab_id, data, tab){
-    if(tab.url.indexOf("google") > -1){
-        chrome.pageAction.show(tab_id);
-    }
-};
-chrome.tabs.onUpdated.addListener(check);
-*/
